fc621a95c6676e7adc95f98124f8dda5ee8da692
danila <terenindanila5@gmail.com>
2025-01-11 08:45:53 +0300

    [WBC1-1] add controllers

--- src/main/java/one/terenin/webfluxconsumer1/configuration/ClientConfiguration.java
+++ src/main/java/one/terenin/webfluxconsumer1/configuration/ClientConfiguration.java
@@ -8,9 +8,20 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class ClientConfiguration {
     @Bean
-    public WebClient webClient() {
+    public WebClient webClientJson() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8080")
+                .baseUrl("http://localhost:8080/json")
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(clientCodecConfigurer -> clientCodecConfigurer
+                                .defaultCodecs()
+                                .maxInMemorySize(8 * 1024 * 1024))
+                        .build())
+                .build();
+    }
+    @Bean
+    public WebClient webClientParquet() {
+        return WebClient.builder()
+                .baseUrl("http://localhost:8080/parquet")
                 .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(clientCodecConfigurer -> clientCodecConfigurer
                                 .defaultCodecs()
--- src/main/java/one/terenin/webfluxconsumer1/controller/DataConsumerController.java
+++ src/main/java/one/terenin/webfluxconsumer1/controller/DataConsumerController.java
@@ -1,12 +1,14 @@
 package one.terenin.webfluxconsumer1.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import one.terenin.webfluxconsumer1.api.Http1Consumer1Api;
 import one.terenin.webfluxconsumer1.consumer.DataConsumerService;
-import one.terenin.webfluxconsumer1.dto.DataBundle;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 
@@ -16,22 +18,19 @@ public class DataConsumerController implements Http1Consumer1Api {
 
     private final DataConsumerService dataConsumerService;
     private final ObjectMapper jacksonObjectMapper;
+    private final WebClient webClientJson;
+    private final WebClient webClientParquet;
 
     @Override
     public Flux<String> streamJsonData() {
-        return dataConsumerService.jsonDataAsFlux().subscribeOn(Schedulers.boundedElastic());/*.filter(it -> {
-            try {
-                // do some work
-                return jacksonObjectMapper.readValue(it, DataBundle.class).getOptions().entrySet().size() > 1;
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-        });*/
+        return dataConsumerService.jsonDataAsFlux()
+                .subscribeOn(Schedulers.boundedElastic());
     }
 
     @Override
     public Flux<byte[]> streamParquetData() {
-        return null;
+        return dataConsumerService.parquetDataAsFlux()
+                .subscribeOn(Schedulers.boundedElastic());
     }
 
     @Override
@@ -46,21 +45,26 @@ public class DataConsumerController implements Http1Consumer1Api {
 
     @Override
     public Flux<String> sizedJsonData(int count) {
-        return null;
+        return  dataConsumerService.jsonDataAsFlux()
+                .limitRate(count)
+                .subscribeOn(Schedulers.boundedElastic());
     }
 
     @Override
     public Flux<byte[]> sizedParquetData(int count) {
-        return null;
+        return dataConsumerService.parquetDataAsFlux()
+                .limitRate(count)
+                .subscribeOn(Schedulers.boundedElastic());
     }
 
     @Override
     public Flux<String> duplexJsonData(int count) {
-        return null;
+        // use Webclient here and make response with it like endless pipe
+        return webClientJson.get().exchangeToFlux(it -> it.bodyToFlux(String.class)).subscribeOn(Schedulers.boundedElastic());
     }
 
     @Override
     public Flux<byte[]> duplexParquetData(int count) {
-        return null;
+        return webClientParquet.get().exchangeToFlux(it -> it.bodyToFlux(byte[].class)).subscribeOn(Schedulers.boundedElastic());
     }
 }
